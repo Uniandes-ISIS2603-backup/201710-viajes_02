ebfa7ed35b30021b739cf7fceb440a5828725673
Your Name <you@example.com>
2017-04-03 19:24:01 -0500

    Se ve bonito :D

--- viajes-02-web/src/main/webapp/src/modules/Automoviles/automoviles.mod.js
+++ viajes-02-web/src/main/webapp/src/modules/Automoviles/automoviles.mod.js
@@ -6,14 +6,14 @@
     var mod = ng.module("automovilesModule",['ui.router']);
     mod.constant("automovilContext","api/automoviles");
     mod.config(['$stateProvider',  '$urlRouterProvider', function ( $stateProvider, $urlRouterProvider) {
-        var basePath = 'src/modules/automoviles/';
+        var basePath = 'src/modules/Automoviles/';
         $urlRouterProvider.otherwise("/automovilesList");
         $stateProvider.state('automoviles',{
             url: '/automoviles',
             abstract: true,
             resolve:
-                { automoviles: ['$http', 'automovilesContext', function( $http, automovilesContext) {
-            return $http.get(automovilesContext);
+                { automoviles: ['$http', function( $http ) {
+                 return $http.get('data/automoviles.json');
 
         }]
     },
@@ -40,16 +40,16 @@
             param: {
                 automovilesId: null
             },
-            resolve:{
-                currentAutomoviles:['$http', 'automovilesContext', '$stateParams', function ($http, automovilesContext, $params) {
-                    return $http.get(automovilesContext+'/'+$params.automovilesId);
-                }]
-            },
+            // resolve:{
+            //     currentAutomoviles:['$http', 'automovilesContext', '$stateParams', function ($http, automovilesContext, $params) {
+            //         return $http.get(automovilesContext+'/'+$params.automovilesId);
+            //     }]
+            // },
             views: {
                 'detailView': {
                     templateUrl: basePath + 'automoviles.detail.html',
                     controller: ['$scope', '$stateParams', function ($scope, $stateParams) {
-                        $scope.currentAutomoviles = $scope.automovilesRecords[$params.automovilesId-1];
+                        $scope.currentAutomoviles = $scope.automovilesRecords[$stateParams.automovilesId-1];
                     }]
                 }
 
