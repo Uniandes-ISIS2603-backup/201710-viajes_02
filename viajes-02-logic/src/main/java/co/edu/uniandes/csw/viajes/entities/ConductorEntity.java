5630277b99a5e18f6b307e8c9639d7d504ef64fb
jm.dominguez <jm.dominguez@ISIS2603-0149.sis.virtual.uniandes.edu.co>
2017-05-10 11:47:44 -0500

    Se arreglan los test de persistencia de conductor

--- viajes-02-logic/src/main/java/co/edu/uniandes/csw/viajes/entities/ConductorEntity.java
+++ viajes-02-logic/src/main/java/co/edu/uniandes/csw/viajes/entities/ConductorEntity.java
@@ -41,36 +41,7 @@ import uk.co.jemos.podam.common.PodamExclude;
 
 @Entity
 public class ConductorEntity extends UsuarioEntity{
-    /**
-     * Identificado del Conductor
-     */
-    @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Long id;
-    /**
-     * Nombre del conductor.
-     */
-    private String nombre;
-    /**
-     * Genero del conductor
-     */
-    private String genero;
-    /**
-     * Telefono del conductor
-     */
-    private Integer telMovil;
-    /**
-     * Edad del conductor
-     */
-    private Integer edad;
-    /**
-     * Rating del conductor
-     */
-    private Double rating;
-    /**
-     * Correo del conductor.
-     */
-    private String correo;
+
     /**
      * Imagen del conductor
      */
@@ -122,105 +93,6 @@ public class ConductorEntity extends UsuarioEntity{
      */
     public void setAutomoviles(List<AutomovilEntity> automoviles) {
         this.automoviles = automoviles;
-    }
-    /**
-     * Retorna el id del conductor.
-     * @return Retorna el id del conductor
-     */
-    public Long getId() {
-        return id;
-    }
-    /**
-     * Asigna un id al conductor.
-     * @param id: identificador del conductor.
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
-    /**
-     * retorna el nombre del conductor.
-     * @return 
-     */
-    public String getNombre() {
-        return nombre;
-    }
-    /**
-     * Asigna un nombre al conductor
-     * @param nombre: Nombre del conductor.
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    /**
-     * Asigna Retorna el genero del conductor.
-     * @return genero del conductor.
-     */
-    @Override
-    public String getGenero() {
-        return genero;
-    }
-    /**
-     * Asigna un genero a el conductor
-     * @param genero del conductor
-     */
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-    /**
-     * Retorna el telefono del conductor
-     * @return Telefono del conductor.
-     */
-    public Integer getTelMovil() {
-        return telMovil;
-    }
-    /**
-     * Asigna un telefono al conductor
-     * @param telMovil: Telefono del conductor
-     */
-    public void setTelMovil(Integer telMovil) {
-        this.telMovil = telMovil;
-    }
-    /**
-     * Retorna la edad del conductor.
-     * @return 
-     */
-    public Integer getEdad() {
-        return edad;
-    }
-    /**
-     * Asigna una edad al conductor
-     * @param edad: Edad del conductor.
-     */
-    public void setEdad(Integer edad) {
-        this.edad = edad;
-    }
-    /**
-     * Retorna el rating del conductor
-     * @return rating del conductor
-     */
-    public Double getRating() {
-        return rating;
-    }
-    /**
-     * Asigna un rating al conductor
-     * @param rating del conductor
-     */
-    public void setRating(Double rating) {
-        this.rating = rating;
-    }
-    /**
-     * Retorna el correo del conductor
-     * @return Correo del conductor
-     */
-    public String getCorreo() {
-        return correo;
-    }
-    /**
-     * Asigna un correo al conductor
-     * @param correo del conductor
-     */
-    public void setCorreo(String correo) {
-        this.correo = correo;
     }
     /**
      * Retorna los reviews del conductor.
