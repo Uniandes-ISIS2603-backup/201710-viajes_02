020a7f3a818bcb6c50b113edc6b5d1d991c1c537
jm.dominguez <jm.dominguez@ISIS2603-0149.sis.virtual.uniandes.edu.co>
2017-05-16 06:24:31 -0500

    Se conecta el formulario de viajes con el backend

--- viajes-02-web/src/main/webapp/src/modules/viajes/viajes.mod.js
+++ viajes-02-web/src/main/webapp/src/modules/viajes/viajes.mod.js
@@ -62,8 +62,90 @@
                 views: {
                     'detailView': {
                         templateUrl: basePath + 'viajes.anadir.html',
-                        controller: ['$scope', 'currentConductor', function ($scope,  currentConductor) {
+                        controller: ['$scope', 'currentConductor', '$http', '$state', 'viajesContext', function ($scope,  currentConductor, $http, $state, viajesContext) {
                                 $scope.currentConductor = currentConductor.data;
+                                $scope.automoviles = $scope.currentConductor.automoviles;
+                                $scope.placa = '';
+                                $scope.origen = {
+                                    id: Math.floor((Math.random() * 100000) + 1),
+                                    direccion: '',
+                                    lat: '',
+                                    lon: '',
+                                    lugar: ''
+                                };
+                                $scope.destino = {
+                                    id: Math.floor((Math.random() * 100000) + 1),
+                                    direccion: '',
+                                    lat: '',
+                                    lon: '',
+                                    lugar: ''
+                                };
+                                $scope.viaje ={
+                                    idViaje: undefined,
+                                    automovil: '',
+                                    conductor: '',
+                                    origen: '',
+                                    destino: '',
+                                    diaYHoraLlegada: '',
+                                    diaYHoraPartida: '',
+                                    gastosCompartidos: '',
+                                    kilometros: ''
+                                };
+                                setAuto = function(placa){
+                                    respuesta = {
+                                        placa : '',
+                                        numSeguro : '',
+                                        modelo : '',
+                                        marca : '',
+                                        compSeguros : '',
+                                        color : '',
+                                        cantAsientos : '',
+                                        id: ''
+                                    };
+                                    automoviles = $scope.automoviles;
+                                    for(var j=0; j < automoviles.length; j++){
+                                        automovil = automoviles[j];
+                                        pplaca = automovil.placa;
+                                        if(placa.includes(pplaca)){
+                                            respuesta.placa = automovil.placa;
+                                            respuesta.numSeguro = automovil.numSeguro;
+                                            respuesta.modelo = automovil.modelo;
+                                            respuesta.marca = automovil.marca;
+                                            respuesta.compSeguros = automovil.compSeguros;
+                                            respuesta.color = automovil.color;
+                                            respuesta.cantAsientos = automovil.cantAsientos;
+                                            respuesta.id = automovil.id;
+                                            return respuesta;
+                                            
+                                        }
+                                    }
+                                };
+                                dateToJson = function(date){
+                                var year = date.charAt(6) + date.charAt(7) + date.charAt(8) + date.charAt(9) ;
+                                var month = date.charAt(3) + date.charAt(4);
+                                var day = date.charAt(1) + date.charAt(0);
+                                var json = year +'-'+month+'-'+day+'T01:01:05.902Z';
+                                    
+                                    
+                                    return json;
+                                    
+                                };
+                                $scope.submit = function(){
+                                    viaje = $scope.viaje;
+                                    viaje.origen = $scope.origen;
+                                    viaje.destino = $scope.destino;
+                                    viaje.conductor = currentConductor.data;
+                                    viaje.automovil = setAuto($scope.placa);
+                                    viaje.diaYHoraPartida = dateToJson(viaje.diaYHoraPartida);
+                                    viaje.diaYHoraLlegada = dateToJson(viaje.diaYHoraLlegada);
+                                    console.log(viaje);
+                                    return $http.post(viajesContext, viaje)
+                                    .then(function () {
+                            // $http.post es una promesa
+                            // cuando termine bien, cambie de estado
+                                    $state.go('viajesList');
+                                    });
+                                };
                             }]
                     }
                 }
